db870d3ccc2001d2beaed75d13a2bae7217d356d
wait <xuweiteng0211@gmail.com>
2022-07-19 09:22:30 +0900

    first commit

---
+++ src/main/java/com/guitu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.guitu.git;
+
+/**
+ * @author 81804
+ * @create 2022-07-15 17:26
+ * @desc
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
